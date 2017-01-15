7543df742993d38e2a241bb1732980b94b235406
Fujimisakari <fujimisakari@gmail.com>
2017-01-15 11:09:33 +0900

    Modify init.sh

--- init.sh
+++ init.sh
@@ -3,9 +3,8 @@
 echo "##### Initialize Start #####"
 
 source .env
-echo "DROP DATABASE IF EXISTS $DB_NAME;"
-echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
-echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
+# echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
 php oil r migrate
 
 echo "insert into todo_categories values (1, 'todo', '2017-01-11 00:00:00', '2017-01-11 00:00:00'), (2, 'do', '2017-01-11 00:00:00', '2017-01-11 00:00:00'), (3, 'done', '2017-01-11 00:00:00', '2017-01-11 00:00:00');" | mysql -u$DB_USER -p$DB_PASS $DB_NAME -h $DB_HOST
